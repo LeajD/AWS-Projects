50c11824f0d045bdd84edea5485ec5db88d9ea74
kikowpio-windows <pkikowski36@gmail.com>
2025-02-18 12:22:21 +0100

    argocd project apps added (rm tfstate)

---
+++ gitops-merging/README.md
@@ -0,0 +1,4 @@
+# GitOps project inside AWS 
+
+Project is based on following guideline:
+https://github.com/antonputra/tutorials/tree/main/lessons/200
---
+++ gitops-merging/terrraform/0-locals.tf
@@ -0,0 +1,9 @@
+locals {
+  env         = "prod"
+  region      = "us-east-1"
+  zone1       = "us-east-1a"
+  zone2       = "us-east-1b"
+  zone3       = "us-east-1c"
+  eks_name    = "demo"
+  eks_version = "1.31"
+}
