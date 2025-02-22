24ec78d1907f88eb1a36e64e72171dc7df10163f
kikowpio-windows <pkikowski36@gmail.com>
2025-02-22 23:07:19 +0100

    new tf deployment

--- terraform-CD/terraform/test.tf
+++ terraform-CD/terraform/test.tf
@@ -23,3 +23,9 @@ resource "aws_instance" "web3tf" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.allow_customip.id]
 }
+resource "aws_instance" "web4tf" {
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = var.web_instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.allow_customip.id]
+}
