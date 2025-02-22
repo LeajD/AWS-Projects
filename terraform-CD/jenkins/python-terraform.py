72c2453b2aca825a0af02514c571e5997d42ea40
kikowpio-windows <pkikowski36@gmail.com>
2025-02-22 22:13:29 +0100

    Jenkinsfile update branch

--- terraform-CD/jenkins/python-terraform.py
+++ terraform-CD/jenkins/python-terraform.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 # Configuration (you can specify it locally or in jenins 'env' section)
 GITHUB_TOKEN=os.environ['GITHUB_TOKEN']
-AWS_ACCESS_KEY_ID=os.environ['aws-access-key-id']
-AWS_SECRET_ACCESS_KEY=os.environ['aws-secret-access-key']
+#AWS_ACCESS_KEY_ID=os.environ['aws-access-key-id']
+#AWS_SECRET_ACCESS_KEY=os.environ['aws-secret-access-key']
 
 REPO_NAME = "LeajD/AWS-Projects"
 BASE_BRANCH = "main"
