0759e14eb78462fd50a193a248b8fa4b758df287
kikowpio-windows <pkikowski36@gmail.com>
2025-02-23 11:55:54 +0100

    new-ci-apply

--- terraform-CD/terraform/test.tf
+++ terraform-CD/terraform/test.tf
@@ -2,8 +2,8 @@ resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-example-bucket-${random_id.bucket_suffix.hex}"
+resource "aws_s3_bucket" "example2" {
+  bucket = "my-example-bucket2-${random_id.bucket_suffix.hex}"
   acl    = "private"
 
   versioning {
@@ -14,18 +14,4 @@ resource "aws_s3_bucket" "example" {
     Name        = "ExampleS3Bucket"
     Environment = "Test"
   }
-}
-
-
-resource "aws_instance" "web3tf" {
-  ami                         = data.aws_ami.amazon_linux_2.id
-  instance_type               = var.web_instance_type
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.allow_customip.id]
-}
-resource "aws_instance" "web4tf" {
-  ami                         = data.aws_ami.amazon_linux_2.id
-  instance_type               = var.web_instance_type
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.allow_customip.id]
 }
